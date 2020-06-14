02e86c6c5fb285e07c53d63a673464deb6fc9c83
anggar <anggarwahyu@gmail.com>
2020-06-14 11:58:55 +0700

    Include post router

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -6,6 +6,7 @@ from .endpoints.comment import router as comment_router
 from .endpoints.profile import router as profile_router
 from .endpoints.tag import router as tag_router
 from .endpoints.user import router as user_router
+from .endpoints.post import router as post_router
 
 router = APIRouter()
 router.include_router(auth_router)
@@ -14,3 +15,4 @@ router.include_router(profile_router)
 router.include_router(comment_router)
 router.include_router(place_router)
 router.include_router(tag_router)
+router.include_router(post_router)
