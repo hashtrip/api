7c2f0ac3cac961910afcdcf74a1a518f2438b638
anggar <anggarwahyu@gmail.com>
2020-06-14 12:19:38 +0700

    Fixing bug in tag and striving for generalization

--- app/services/tag.py
+++ app/services/tag.py
@@ -14,13 +14,13 @@ async def fetch_all_tags(conn: AsyncIOMotorClient) -> List[TagInDB]:
     return tags
 
 
-async def get_tags_for_place(conn: AsyncIOMotorClient, slug: str) -> List[TagInDB]:
+async def get_tags(conn: AsyncIOMotorClient, slug: str, doc_name: str = place_collection_name) -> List[TagInDB]:
     tags = []
-    place_tags = await conn[database_name][place_collection_name].find_one(
+    place_tags = await conn[database_name][doc_name].find_one(
         {"slug": slug}, projection={"tag_list": True}
     )
     for row in place_tags["tag_list"]:
-        tags.append(TagInDB(tag={"tag": row}))
+        tags.append(TagInDB(tag=row))
 
     return tags
 
@@ -28,10 +28,10 @@ async def get_tags_for_place(conn: AsyncIOMotorClient, slug: str) -> List[TagInD
 async def create_tags_that_not_exist(conn: AsyncIOMotorClient, tags: List[str]):
     dbconn = conn[database_name][tags_collection_name]
     existing = await dbconn.find({}, projection={"_id": False}).to_list(None)
-    existing = map(lambda x: x["tag"], existing)
+    existing = list(map(lambda x: x["tag"], existing))
 
     filtered = list(filter(lambda x: x not in existing, tags))
 
     await dbconn.insert_many(
         [{"tag": tag} for tag in filtered]
-    )
+    ) if filtered else None
