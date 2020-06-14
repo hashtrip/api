fbe05d217479eb3cdd1a8850275913cb8969e4d9
anggar <anggarwahyu@gmail.com>
2020-06-14 11:58:14 +0700

    Deprecating usage for Schema in favor of Field for Pydantic

--- app/models/dbmodel.py
+++ app/models/dbmodel.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 from typing import Optional
 
-from pydantic import BaseModel, Schema
+from pydantic import BaseModel, Field
 
 
 class DateTimeModelMixin(BaseModel):
-    created_at: Optional[datetime] = Schema(None, alias="createdAt")
-    updated_at: Optional[datetime] = Schema(None, alias="updatedAt")
+    created_at: Optional[datetime] = Field(None, alias="createdAt")
+    updated_at: Optional[datetime] = Field(None, alias="updatedAt")
 
 
 class DBModelMixin(DateTimeModelMixin):
